af78fd8ee88bf0f81bbd50b36425812e061d4dd7
TonyToscana <xxpoblanozxx@gmail.com>
2019-05-12 18:03:13 +0200

    Iniciate MainMenu and possible new Player's animations

---
+++ Assets/Scripts/Character/CharacterController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float speed = 4.4f;
+    public Animator animator;
+
+
+    private CommandInvoker commandInvoker;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+    }
+}
