166ec0a9e4ff073cc056863da8985f9e9c9d8119
TonyToscana <xxpoblanozxx@gmail.com>
2019-05-16 22:13:50 +0200

    Add Pause method to AudioManager and isPaused property to Sound class

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -40,11 +40,45 @@ public class AudioManager : MonoBehaviour
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
         s?.source.Play();
+        s.isPaused = false;
     }
 
     public void Stop(string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
         s?.source.Stop();
+    }
+
+    public void Pause(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s?.source.Pause();
+        s.isPaused = true;
+    }
+
+    public void PauseAllPlaying()
+    {
+        foreach (Sound s in sounds)
+        {
+            if(s.source.isPlaying)
+            {
+                s.source.Pause();
+                s.isPaused = true;
+            }
+                
+        }
+    }
+
+    public void ResumePaused()
+    {
+        foreach (Sound s in sounds)
+        {
+            if(s.isPaused)
+            {
+                s.source.Play();
+                s.isPaused = false;
+            }
+                
+        }
     }
 }
--- Assets/Scripts/Audio/Sound.cs
+++ Assets/Scripts/Audio/Sound.cs
@@ -17,6 +17,8 @@ public class Sound
 
     public bool loop;
 
+    public bool isPaused;
+
     [HideInInspector]
     public AudioSource source;
 }
