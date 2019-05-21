c3e29511cf1b3bde827e9744b42a469b00810af8
velikGeorgiev <velko.chelebiev@gmail.com>
2019-05-21 23:15:44 +0200

    fixes and blackhole

---
+++ Assets/RandomMissile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMissile : MonoBehaviour
+{
+    [SerializeField] public string shootableTag = "";
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.tag.Equals(this.shootableTag)) return;
+
+        Health health = collision.GetComponent<Health>();
+
+        health.Damage(30);
+
+        Destroy(this.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/RandomShooter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomShooter : MonoBehaviour
+{
+    [SerializeField] public GameObject missile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Shoot(UnityEngine.Random.Range(3, 7)));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private IEnumerator Shoot(int v)
+    {
+        yield return new WaitForSeconds(v);
+        int missileNumber = UnityEngine.Random.Range(1, 5);
+
+        for (int i = 0; i < missileNumber; i++)
+        {
+            GameObject missile = Instantiate(this.missile, this.transform.position, Quaternion.identity);
+            missile.transform.parent = null;
+
+            Vector2 dir = new Vector2(UnityEngine.Random.Range(-10, 10), UnityEngine.Random.Range(-10, 10));
+
+            missile.GetComponent<Rigidbody2D>().AddForce(dir * 30);
+
+            yield return new WaitForSeconds(UnityEngine.Random.Range(0, 1.5f));
+        }
+
+        Destroy(this.gameObject);
+    }
+}
