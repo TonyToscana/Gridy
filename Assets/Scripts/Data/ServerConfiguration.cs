74148da1c6239d4adca054c67f5af28a49a086c1
TonyToscana <xxpoblanozxx@gmail.com>
2019-06-13 22:17:04 +0200

    Update ServerConfiguration.cs

--- Assets/Scripts/Data/ServerConfiguration.cs
+++ Assets/Scripts/Data/ServerConfiguration.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ServerConfiguration 
 {
-    public static string BaseUrl => "http://127.0.0.1:9081/";
-    public static string ScoreUrl => "http://127.0.0.1:9081/rest/score";
+    public static string BaseUrl => "http://127.0.0.1:9082/";
+    public static string ScoreUrl => "http://127.0.0.1:9082/rest/score";
     public static string ScorePath => "/rest/score";
 }
