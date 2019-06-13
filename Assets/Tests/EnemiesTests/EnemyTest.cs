437c2bfc17b6e8d3f592519feb9ef33bec296faf
TonyToscana <xxpoblanozxx@gmail.com>
2019-06-13 13:19:53 +0200

    Add EnemiesTests

---
+++ Assets/Tests/EnemiesTests/EnemyTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using NSubstitute;
+
+namespace Tests
+{
+    public class EnemyTest
+    {
+        [Test]
+        public void TestGetHealthMethod()
+        {
+            EnemyLogic enemy = new EnemyLogic();
+
+            Assert.AreEqual(100, enemy.GetHealth());
+        }
+        // A Test behaves as an ordinary method
+        [Test]
+        public void EnemyRecievesDamageCorrectly()
+        {
+            EnemyLogic enemy = new EnemyLogic();
+            enemy.Damage(10);
+
+            Assert.AreEqual(90, enemy.GetHealth());
+        }
+    }
+}
